69340ccd287e39bebec80cf865cade22cf8bb590
Lo Fl <lflora@cub.uca.edu>
2025-04-08 10:57:49 -0500

    No longer using linkedLists

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
-#include "LinkedList.h"
-
+#include <sstream>
+#include <string>
 using namespace std;
 
 //File Reader
@@ -11,11 +11,11 @@ using namespace std;
 //       algo - reference to the algorithm name
 //Output: VOID
 //Description: Reads a file and store contents
-void fileReader(string fileName, LinkedList<int> &ref_List, int &num_frames, string &algo) {
+int* fileReader(string fileName, int &num_frames, string &algo, int &str_size) {
     ifstream file(fileName);
     if (!file.is_open()) {
         cout << "Error opening file: " << fileName << endl;
-        return;
+        
     }
     //Read the first and only line of the file
     string line;
@@ -32,16 +32,61 @@ void fileReader(string fileName, LinkedList<int> &ref_List, int &num_frames, str
     string num_frames_str;
     getline(ss, num_frames_str, ',');
     num_frames = stoi(num_frames_str);
-    
+
+    //Get size of reference string
+    int size = 0;
+    while (getline(ss, line, ',')) {
+        size++;
+    }
+    //Account for 2 parameters at beginning
+    size -= 2; 
+    str_size = size;
+
     //Get the rest of the string (reference string)
+    int* temp_list = new int[size]; 
+    int i = 0; 
     string page_number_s;
     while (getline(ss, page_number_s, ',')) {
+        //Change to int
         int page_number = stoi(page_number_s);
-        ref_List.push_back(page_number);
+        //Store in the array
+       temp_list[i] = page_number;
+       //Array counter 
+        i++;
     }
+
     //Values are stored by reference
+    return temp_list;
 }
 
+void OPT(int* ref_List, int num_frames) {
+    int* curr_frame = new int[num_frames]; 
+    //Iterate through the reference list
+    for(int i = 0; i < sizeof(ref_List); i++) {
+        //Store current page number and remove from list
+        //For next iteration, as we'll check new front
+        int curr_page = ref_List[i];
+        
+        //Check if the page is already in the frame
+        for(int j = 0; i < sizeof(curr_frame); i++) {
+            //Check if the page is in the frame
+            if (curr_frame[j] == curr_page) {
+                //Page is already in the frame, so break out of the loop
+                break; 
+            }
+            
+        }
+        //Check if the frame is full
+        for(int j = 0; j < num_frames; j++) {
+            //Check if the frame is empty
+            if (curr_frame[j] == -1) {
+                //Add the page to the frame
+                curr_frame[j] = curr_page;
+                break; 
+            }
+        }
+    }
+}
 
 //Main Function
 int main() {
@@ -52,16 +97,30 @@ int main() {
     //Initialize variables
     string algo = " "; 
     int num_frames = 0;
-    LinkedList<int> ref_List;
+    int str_size = 0;
 
     //Read in and store file contents
-    fileReader(oFile, ref_List, num_frames, algo); 
+    int* ref_List = fileReader(oFile, num_frames, algo, str_size); 
 
-
-    //Print the Reference String
+    //Print the Reference String (First Part of Output)
     cout << "Reference String: " << endl; 
-    cout << ref_List.toString() << endl;
+    
+    for (int i = 0; i < sizeof(ref_List); i++) {
+        cout << ref_List[i] << " "; 
+    }
 
+    if(algo == "O"){
 
+    }
+    else if (algo == " ")//Whichever of the two u chose
+    {
+    
+    }
+    else {//Invalid Algorithm
+        cout << "Invalid Algorithm: " + algo << endl;
+        return 1;
+    }
+
+    cout << "page frames" << endl; //Final part of Output
 }
 
