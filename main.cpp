8a37aa4cd7412f07016e6bc8d3ec175250a50862
Lo Fl <lflora@cub.uca.edu>
2025-04-08 11:39:20 -0500

    Finished optimal algo, working on printing the 2D table, which will finish the output

--- main.cpp
+++ main.cpp
@@ -59,33 +59,93 @@ int* fileReader(string fileName, int &num_frames, string &algo, int &str_size) {
     return temp_list;
 }
 
-void OPT(int* ref_List, int num_frames) {
-    int* curr_frame = new int[num_frames]; 
+int** OPT(int* ref_List, int num_frames, int str_size) {
+    //Table to hold all frames
+    int** frame_table = new int*[str_size];
+    for(int i = 0; i < str_size; i++) {
+        frame_table[i] = new int[num_frames]; 
+    }
+ 
     //Iterate through the reference list
-    for(int i = 0; i < sizeof(ref_List); i++) {
-        //Store current page number and remove from list
-        //For next iteration, as we'll check new front
+    for(int i = 0; i < str_size; i++) {
+        int* curr_frame = new int[num_frames]; 
+        //Holds the current frame to be stored in table
+        if (i != 0)
+            curr_frame = frame_table[i-1];
+        
+
+        //Store current page number 
         int curr_page = ref_List[i];
         
         //Check if the page is already in the frame
-        for(int j = 0; i < sizeof(curr_frame); i++) {
+        bool page_found = 0;
+        for(int j = 0; i < num_frames; i++) {
             //Check if the page is in the frame
             if (curr_frame[j] == curr_page) {
                 //Page is already in the frame, so break out of the loop
+                page_found = 1;
                 break; 
             }
             
         }
-        //Check if the frame is full
-        for(int j = 0; j < num_frames; j++) {
-            //Check if the frame is empty
-            if (curr_frame[j] == -1) {
-                //Add the page to the frame
-                curr_frame[j] = curr_page;
-                break; 
+
+        //Check if the frame is full, add to the frame if not
+        bool frame_full = 1;
+        if(!page_found){
+            for(int j = 0; j < num_frames; j++) {
+                //Check if the frame is empty
+                if (curr_frame[j] == -1) {
+                    //Add the page to the frame
+                    curr_frame[j] = curr_page;
+                    //Add to table as thats the end of this iteration
+                    frame_table[i] = curr_frame;
+                    frame_full = 0;
+                    break; 
+                }
+            }
+        }
+
+        //If the frame is full, we need to replace a page
+        if(frame_full) {
+
+            //Find the page to replace
+            int page_to_replace = -1; 
+            int farthest_index = -1; 
+            for(int j = 0; j < num_frames; j++) {
+                //Find the farthest page in the future
+                for(int k = i + 1; k < sizeof(ref_List); k++) {
+                    if (curr_frame[j] == ref_List[k]) {
+                        //Check if this is the farthest page
+                        if (k > farthest_index) {
+                            farthest_index = k;
+                            page_to_replace = curr_frame[j];
+                        }
+                    }
+                }
+            }
+
+            //Replace the page
+            for(int j = 0; j < num_frames; j++) {
+                if (curr_frame[j] == page_to_replace) {
+                    curr_frame[j] = curr_page;
+                    //Add to table as thats the end of this iteration
+                    frame_table[i] = curr_frame;
+                    break; 
+                }
             }
         }
     }
+    return frame_table;
+}
+
+//Prints the Frame Table
+//Input: frame_table - the table to print
+//       num_frames - the number of frames
+//       str_size - the size of the reference string
+//Output: VOID
+//Description: Prints the frame table to the console
+void printTable(int** frame_table, int num_frames, int str_size) {
+   
 }
 
 //Main Function
@@ -105,12 +165,18 @@ int main() {
     //Print the Reference String (First Part of Output)
     cout << "Reference String: " << endl; 
     
-    for (int i = 0; i < sizeof(ref_List); i++) {
+    for (int i = 0; i < str_size; i++) {
         cout << ref_List[i] << " "; 
     }
 
-    if(algo == "O"){
+    //Initialize Frame Table 
+    int** frame_table = new int*[str_size]; //Table to hold all frames
+    for(int i = 0; i < str_size; i++) {
+        frame_table[i] = new int[num_frames]; 
+    }
 
+    if(algo == "O"){
+        frame_table = OPT(ref_List, num_frames, str_size);
     }
     else if (algo == " ")//Whichever of the two u chose
     {
@@ -121,6 +187,7 @@ int main() {
         return 1;
     }
 
+    printTable(frame_table, num_frames, str_size); 
     cout << "page frames" << endl; //Final part of Output
 }
 
